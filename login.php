7bc82d115df79cfa135d6fecabd55336d88d3465
TrinhQuangTan <supermouse0985@gmail.com>
2024-03-15 09:07:29 +0700

    Thay đổi label username và password

--- login.php
+++ login.php
@@ -9,9 +9,9 @@
 <div class="login-container">
     <h1>login</h1>
     <form action="result.php" method="post">
-        <label for="username">Username:</label>
+        <label for="username">Tên đăng nhậ:</label>
         <input type="text" id="username" name="username" required>
-        <label for="password">Password:</label>
+        <label for="password">Mật Khẩu:</label>
         <input type="password" id="password" name="txtpassword" required>
         <button type="submit">Login</button>
     </form>
