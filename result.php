d59d2a9594bc5b968f9c6f8afaebd3f11f764304
TrinhQuangTan <supermouse0985@gmail.com>
2024-03-15 09:16:47 +0700

    Thêm chức năng login từ result.php và thêm style.css

--- result.php
+++ result.php
@@ -2,10 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    
+
+<div class="login-container">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        if ($username == "admin" && $password == "admin") {
+            echo '<div style="color: red; font-family: Tahoma;
+            ">Đăng nhập thành công</div>';
+        } else {
+            echo '<div style="color: red; font-family: Tahoma;
+            ">Tên đăng nhập/mật khẩu sai. Vui lòng nhập lại.</div>';
+        }
+    }
+    ?>
+</div>
+
 </body>
 </html>
